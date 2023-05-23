ee57ae85d13be063c6a181dcf3212f483d84260b
Jaohar Raihan <80238860+JaoharRaihan@users.noreply.github.com>
2023-05-23 16:40:39 +0600

    s

--- app/crop_disease/prediction/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Prediction
-
-class PredictionForm(forms.ModelForm):
-    class Meta:
-        model = Prediction
-        fields = ('image',)
--- app/crop_disease/prediction/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Prediction(models.Model):
-    image = models.ImageField(upload_to='images/')
-    result = models.CharField(max_length=100)
